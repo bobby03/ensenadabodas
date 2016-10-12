801587f3f07f123124e9defca3f9c4ed8e157b59
MidoriHeart <midori.wings@gmail.com>
2016-10-12 16:43:46 -0700

    contacto/nosotros.

---
+++ protected/views/acercade/index.php
@@ -0,0 +1,22 @@
+<?php
+/* @var $this AcercadeController */
+$baseUrl = Yii::app()->baseUrl;
+    $cs = Yii::app()->getClientScript();
+    $cs->registerCoreScript('jquery');
+    $cs->registerCssFile($baseUrl.'/css/acercade/nosotros.css');
+
+?>
+<div class="content-wrapper"> 
+	<div class="container1">
+		<label class="titulo1">ACERCA DE </label><label class="titulo2">ENSENADA BODAS</label>	
+	</div>
+	<div class="container2">
+
+	Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor
+	volutpat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. 
+	Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin 
+	molestie malesuada. Quisque velit nisi, pretium ut lacinia in, elementum id enim. 
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis 
+	in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor 
+	sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget.
+	 malesuada. Donec rutrum congue leo eget malesuada. </div>
